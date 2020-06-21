56497f7296030abffb2bfaa8006add74de07c584
Felipe Trevisan <fel.trevisan@gmail.com>
2020-06-20 23:49:44 -0300

    Remoção do Loggly e regatoração do logger

--- src/helpers/logger.js
+++ src/helpers/logger.js
@@ -1,6 +1,5 @@
 const winston = require('winston');
 const Logsene = require('winston-logsene');
-const { Loggly } = require('winston-loggly-bulk');
 
 const transform = (toBeFormatted) => {
   if (typeof toBeFormatted.message === 'string') {
@@ -11,50 +10,26 @@ const transform = (toBeFormatted) => {
 
 const logger = winston.createLogger({
   levels: winston.config.npm.levels,
-  transports: [],
+  transports: [
+    new winston.transports.Console({
+      colorize: true,
+      handleExceptions: true,
+      level: process.env.LOG_CONSOLE_LEVEL,
+      prettyPrint: true,
+      silent: false,
+    }),
+  ],
   silent: process.env.LOG_SILENT === 'true',
 });
 
-if (process.env.LOG_CONSOLE === 'true') {
-  const logTransport = new winston.transports.Console({
-    colorize: true,
-    handleExceptions: true,
-    level: process.env.LOG_CONSOLE_LEVEL,
-    prettyPrint: true,
-    silent: false,
-  });
-
-  if (!logger.transports.includes(logTransport)) {
-    logger.add(logTransport);
-  }
-}
-
-if (process.env.ENABLE_LOGGLY === 'true') {
-  const logglyTransport = new Loggly({
-    token: process.env.LOGS_LOGGLY_KEY,
-    subdomain: process.env.LOGS_LOGGLY_SUBDOMAIN,
-    json: true,
-    format: { transform },
-    level: process.env.LOG_EXTERNAL_LEVEL,
-  });
-
-  if (!logger.transports.includes(logglyTransport)) {
-    logger.add(logglyTransport);
-  }
-}
-
 if (process.env.ENABLE_SEMATEXT === 'true') {
-  const logseneTransport = new Logsene({
+  logger.add(new Logsene({
     token: process.env.LOG_SEMATEXT_KEY,
     url: process.env.LOG_SEMATEX_URL,
     format: { transform },
     json: true,
     level: process.env.LOG_EXTERNAL_LEVEL,
-  });
-
-  if (!logger.transports.includes(logseneTransport)) {
-    logger.add(logseneTransport);
-  }
+  }));
 }
 
 module.exports = logger;
